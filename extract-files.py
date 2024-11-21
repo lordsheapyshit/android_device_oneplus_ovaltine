969f7f5dd37ceb22ba8dcaf410dd5dab25b88fd4
lordsheapshyt <lordsheapshyt@proton.me>
2024-11-21 15:07:05 +0000

    ovaltine: Add ovaltine specific fixes
    Change-Id: I154b187db5cbc41de1d36f1e07fc6717e11d607d

--- extract-files.py
+++ extract-files.py
@@ -29,8 +29,27 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
+    'odm/bin/hw/vendor.oplus.hardware.cammidasservice-V1-service': blob_fixup()
+        .replace_needed('android.frameworks.stats-V1-ndk_platform.so', 'android.frameworks.stats-V2-ndk.so'),
+    (
+        "odm/lib64/libaps_frame_registration.so",
+        "odm/lib64/libCOppLceTonemapAPI.so",
+        "odm/lib64/libCS.so",
+        "odm/lib64/libSuperRaw.so",
+        "odm/lib64/libYTCommon.so",
+        "odm/lib64/libyuv2.so",
+    ): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'odm/lib64/libAlgoProcess.so': blob_fixup()
         .replace_needed('android.hardware.graphics.common-V2-ndk_platform.so', 'android.hardware.graphics.common-V5-ndk.so'),
+    'odm/lib64/libextensionlayer.so': blob_fixup()
+        .replace_needed('libziparchive.so', 'libziparchive_odm.so'),
+    'vendor/lib64/libcamximageformatutils.so': blob_fixup()
+        .replace_needed('vendor.qti.hardware.display.config-V2-ndk_platform.so', 'vendor.qti.hardware.display.config-V2-ndk.so'),
+
+    # regex
+    'odm/etc/camera/CameraHWConfiguration.config': blob_fixup()
+        .regex_replace('SystemCamera = 0;  0;  0;  1;  0;  1;', 'SystemCamera = 0;  0;  0;  0;  0;  1;'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
